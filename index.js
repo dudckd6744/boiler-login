00184383af811446f8be1c8e4e8c3d34854c1a19
우영창 <dudckd@uyeongchang-ui-MacBookPro.local>
2020-07-05 11:43:29 +0900

    저장소2

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+const {User} = require('./models/User');
+
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.use(bodyparser.json())
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://dudckd:djajsl11@youtb-mdyrj.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser:true,  useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req,res) => {
+    res.send('hellow')
+})
+
+app.post('/register', (req,res)=> {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+app.listen(5000, () => console.log(`Example app listening in port 5000!!`))
